14312006b8f03b6c936cbb9166163c44449aa78f
Adrian <eslenderdark@gmail.com>
2025-06-03 22:37:06 +0200

    localhost

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,22 +1,22 @@
 import { Pool } from 'pg';
-/*
+
 const pool = new Pool({
   user: 'postgres',
   password: '1234',
   host: 'localhost',
   port: 5432, // default Postgres port
   database: 'RPG'
-});*/
-
-
-const pool = new Pool({
-  user: 'rpg_bdd_user',
-  password: '1Bs3FNjSY1XXeSbjkAI8kXITKOArgq6r',
-  host: 'dpg-d0m93i95pdvs7391s2d0-a',
-  port: 5432,
-  database: 'rpg_bdd',
-  
 });
+
+
+// const pool = new Pool({
+//   user: 'rpg_bdd_user',
+//   password: '1Bs3FNjSY1XXeSbjkAI8kXITKOArgq6r',
+//   host: 'dpg-d0m93i95pdvs7391s2d0-a',
+//   port: 5432,
+//   database: 'rpg_bdd',
+  
+// });
 
 
 
